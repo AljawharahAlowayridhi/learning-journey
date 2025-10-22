c35403af443a43cc66de94079ffa19455af2a1c7
aljawharah <aljoharahali70@outlook.com>
2025-10-22 15:57:29 +0300

    Initial Commit

---
+++ learning journey/learning_journeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_journeyApp.swift
+//  learning journey
+//
+//  Created by aljawharah alowayridhi on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct learning_journeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learning journeyTests/learning_journeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  learning_journeyTests.swift
+//  learning journeyTests
+//
+//  Created by aljawharah alowayridhi on 30/04/1447 AH.
+//
+
+import Testing
+@testable import learning_journey
+
+struct learning_journeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
